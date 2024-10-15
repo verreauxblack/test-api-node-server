1c15763a346fe3dd05feaeb044bae72f3499de60
raj.kumar <rajkumar@dronamobile.com>
2024-10-15 14:03:09 +0530

    pdf files added and folder organized

--- index.js
+++ index.js
@@ -15,58 +15,71 @@ const generateToken = (user) => {
   return jwt.sign({ username: user.username }, JWT_SECRET, { expiresIn: user.expiresIn || '5m' });
 };
 const logRequestUrlMiddleWare = async (req, res, next) => {
-    console.log("request for" + req.originalUrl)
-    next();
+  console.log("request for" + req.originalUrl)
+  next();
 }
 
 app.use(logRequestUrlMiddleWare);
 // REST API route for login
 app.post('/login', (req, res) => {
-    const { username, password } = req.body;
-    if (!username || !password) {
-      return res.status(400).json({ message: 'Username and password are required' });
-    }
-    const expiresIn = req.query.expiresIn;
-  
-    if (username === validUsername && password === validPassword) {
-      const token = generateToken({ username, expiresIn });
-      return res.json({ token });
-    } else {
-      return res.status(401).json({ message: 'Invalid credentials' });
-    }
-  });
-  
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(400).json({ message: 'Username and password are required' });
+  }
+  const expiresIn = req.query.expiresIn;
+
+  if (username === validUsername && password === validPassword) {
+    const token = generateToken({ username, expiresIn });
+    return res.json({ token });
+  } else {
+    return res.status(401).json({ message: 'Invalid credentials' });
+  }
+});
+
 
 // Middleware to check JWT token before the request reaches Apollo Server
 const authenticateToken = (req, res, next) => {
-    const authHeader = req.headers.authorization || '';
-    if (authHeader) {
-      const token = authHeader.split(' ')[1]; // Extract token from "Bearer <token>"
-      try {
-        const decoded = jwt.verify(token, JWT_SECRET);
-        req.user = decoded;
-        next();
-      } catch (err) {
-        return res.status(401).json({ message: 'Invalid or expired token' });
-      }
-    } else {
-      return res.status(401).json({ message: 'Authorization header missing' });
+  const authHeader = req.headers.authorization || '';
+  if (authHeader) {
+    const token = authHeader.split(' ')[1]; // Extract token from "Bearer <token>"
+    try {
+      const decoded = jwt.verify(token, JWT_SECRET);
+      req.user = decoded;
+      next();
+    } catch (err) {
+      return res.status(401).json({ message: 'Invalid or expired token' });
     }
-  };
-  
-app.get("/downloadFile", authenticateToken, (req, res) => {
-    if (req.query.fileType === "jpg" ) return res.download("./onepiece_ace2.jpg");
-    return res.download("./sample.txt");
-});
+  } else {
+    return res.status(401).json({ message: 'Authorization header missing' });
+  }
+};
 
-app.get("/no_auth/downloadFile", (req, res) => {
-    if (req.query.fileType === "jpg" ) return res.download("./onepiece_ace2.jpg");
-    return res.download("./sample.txt");
-});
+function downloadFile(req, res) {
+  let filePath = "./files/sample.txt";
+  switch (req.query.fileType) {
+    case "jpg":
+      filePath = "./files/onepiece_ace2.jpg";
+      break;
+    case "pdf":
+      filePath = "./files/g-economicwild.pdf";
+      if (req.query.size === "medium") {
+        filePath = "./files/One-Piece-Volume-001.pdf";
+      }
+      break;
+    default:
+      filePath = "./files/sample.txt";
+      break;
+  }
+  return res.download(filePath);
+}
+
+app.get("/downloadFile", authenticateToken, downloadFile);
+
+app.get("/no_auth/downloadFile", downloadFile);
 
 const port = process.env.PORT || 3000;
 
 // Start the server and listen on the specified port
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
