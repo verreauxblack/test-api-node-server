452f2847a77f0e65824c40aa305babb4fda9e061
RAJ <verreauxblack@outlook.com>
2025-02-27 20:35:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -69,9 +69,13 @@ function downloadFile(req, res) {
     case "yobi_pdf":
       filePath = "./files/WilfulDefaultForm_67aafa57c2a1b6477ef97b4d_s3.pdf";
       break;
-    default:
+    case "text:
       filePath = "./files/sample.txt";
       break;
+    default:
+      filePath = "./files/sample.txt";
+      res.status(404);
+      break;      
   }
   return res.download(filePath);
 }
